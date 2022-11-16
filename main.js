037daa4d45e01a5dab416ab536cb307c7f84c032
Vic <vicjs612@gmail.com>
2022-11-15 20:49:23 -0600

    git commit for Jaziel part

--- main.js
+++ main.js
@@ -1,5 +1,61 @@
 // loops practices
-
+const person = [
+    {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      birthDate: 'Jan 5, 1925',
+      gender: 'female'
+    },
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      birthDate: 'June 11, 1991',
+      year: '1991',
+      month: '6',
+      gender: 'Male'
+    },
+    {
+      firstName: 'Jahn',
+      lastName: 'Doe',
+      birthDate: 'Sep 20, 1989',
+      year: '1989',
+      month: '9',
+      gender: 'Female'
+    },
+    {
+      firstName: 'Jone',
+      lastName: 'Dino',
+      birthDate: 'Nov 2, 1990',
+      year: '1990',
+      month: '11',
+      gender: 'Male'
+    },
+    {
+      firstName: 'Jhan',
+      lastName: 'Dino',
+      birthDate: 'Feb 13, 2010',
+      year: '2010',
+      month: '2',
+      gender: 'Female'
+    },
+    {
+      firstName: 'Jeon',
+      lastName: 'Dino',
+      birthDate: 'May 19, 2002',
+      year: '2002',
+      month: '5',
+      gender: 'Male'
+    },
+    {
+      firstName: 'Jean',
+      lastName: 'Ode',
+      birthDate: 'Sep 10, 1970',
+      year: '1970',
+      month: '9',
+      gender: 'Female'
+    }
+  ]
+  
 // **** Vic Part ****
 // do...while loop to count from 1 to 1000
 let i = 1;
@@ -8,12 +64,7 @@ do {
  i++
  } while (i <= 1000)
 
-const person = {
-  firstName: 'Jane',
-  lastName: 'Doe',
-  birthDate: 'Jan 5, 1925',
-  gender: 'female'
-}
+
 // Object.keys() 
 function getObjKey() {
   let key1 = Object.keys(person);
@@ -31,3 +82,19 @@ getEntry()
 // *** Elias part ***
 
 // *** Jaziel part ***
+var male = person.filter(type => type.gender === 'Male');
+ 
+var female = person.filter(type => type.gender === 'Female');
+ 
+const old = person.filter(person => {
+if (person.year >= 1900 && person.year <= 1989) {
+return true;
+}
+});
+console.log (old)
+
+let data = person.map(function(element){
+    return ${element.firstName} ${element.lastName} ${element.birthDate} ${element.year} ${element.month} ${element.gender};
+    })
+    
+    console.log(data);
