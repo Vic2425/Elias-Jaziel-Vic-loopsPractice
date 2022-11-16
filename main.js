7c6bb45b89d02ed758336f3f76efe935d88bfafb
Vic <vicjs612@gmail.com>
2022-11-15 20:23:24 -0600

    git commit for Vic part

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 // loops practices
+
+// **** Vic Part ****
+// do...while loop to count from 1 to 1000
+let i = 1;
+do {
+ console.log(i)
+ i++
+ } while (i <= 1000)
+
+const person = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  birthDate: 'Jan 5, 1925',
+  gender: 'female'
+}
+// Object.keys() 
+function getObjKey() {
+  let key1 = Object.keys(person);
+  console.log(key1)
+}
+getObjKey()
+
+// Object.entries()
+function getEntry() {
+  let personEntry = Object.entries(person);
+  console.log(personEntry);
+}
+getEntry()
+
+// *** Elias part ***
+
+// *** Jaziel part ***
